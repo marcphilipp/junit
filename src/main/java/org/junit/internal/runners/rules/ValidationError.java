a170a3d8d1729c81a2d304ccd6fd547f19e7add9
Marc Philipp <mail@marcphilipp.de>
2014-12-08 21:02:30 +0100

    Suppress warnings in junit.* code

--- src/main/java/org/junit/internal/runners/rules/ValidationError.java
+++ src/main/java/org/junit/internal/runners/rules/ValidationError.java
@@ -4,6 +4,7 @@ import org.junit.runners.model.FrameworkMember;
 
 import java.lang.annotation.Annotation;
 
+@SuppressWarnings("serial")
 class ValidationError extends Exception {
     public ValidationError(FrameworkMember<?> member, Class<? extends Annotation> annotation, String suffix) {
         super(String.format("The @%s '%s' %s", annotation.getSimpleName(), member.getName(), suffix));
