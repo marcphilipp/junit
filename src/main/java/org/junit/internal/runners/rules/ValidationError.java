08e6b867efcbc5c0b3d97ae2c1eab78f2318c021
Marc Philipp <mail@marcphilipp.de>
2014-12-14 09:38:41 +0100

    Add serialver generated serialVersionUID

--- src/main/java/org/junit/internal/runners/rules/ValidationError.java
+++ src/main/java/org/junit/internal/runners/rules/ValidationError.java
@@ -4,8 +4,10 @@ import org.junit.runners.model.FrameworkMember;
 
 import java.lang.annotation.Annotation;
 
-@SuppressWarnings("serial")
 class ValidationError extends Exception {
+
+    private static final long serialVersionUID = 3176511008672645574L;
+
     public ValidationError(FrameworkMember<?> member, Class<? extends Annotation> annotation, String suffix) {
         super(String.format("The @%s '%s' %s", annotation.getSimpleName(), member.getName(), suffix));
     }
